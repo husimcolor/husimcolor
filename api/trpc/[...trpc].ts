cb0a2205eb308e57d83565f6aca8aa24b049f77c
husimcolor <leeya1577@gmail.com>
2026-09-16 10:17:49 +0000

    fix: route legacy admin auth through trpc function

--- api/trpc/[...trpc].ts
+++ api/trpc/[...trpc].ts
@@ -10,6 +10,7 @@ import express from "express";
 import { appRouter } from "../../server/routers";
 import { createContext } from "../../server/_core/context";
 import { createOAuthLoginUrl, getProductionFrontendOrigin } from "../../server/_core/oauth";
+import { registerLegacyAdminRoutes } from "../../server/_core/legacy-admin";
 import { ENV } from "../../server/_core/env";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
@@ -17,6 +18,7 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+registerLegacyAdminRoutes(app);
 
 // CORS 설정
 app.use((req, res, next) => {
